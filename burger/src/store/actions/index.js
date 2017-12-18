366d98da8e555b34631f22308a75ad11f7ce415f
gmartinez31 <bhgustavo13@yahoo.com>
2017-12-18 13:57:55 -0600

    fetching orders via redux

--- burger/src/store/actions/index.js
+++ burger/src/store/actions/index.js
@@ -1,2 +1,2 @@
 export { addIngredient, deleteIngredient, initIngredients } from './burgerBuilder';
-export { purchaseBurger, purchaseInit } from './order';
+export { purchaseBurger, purchaseInit, fetchOrders } from './order';
